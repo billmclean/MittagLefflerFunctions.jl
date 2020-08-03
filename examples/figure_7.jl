9931651e19fae4bd1a2aeb48a43ad1ce13f12405
William McLean <w.mclean@unsw.edu.au>
2020-08-03 17:11:50 +1000

    tweak y-axis scale in fig. 7

--- examples/figure_7.jl
+++ examples/figure_7.jl
@@ -6,7 +6,7 @@ using SpecialFunctions: erfcx
 β = 1.0
 
 #x = range(0, 10, length=201)
-x = range(-5, 3, length=201)
+x = range(-5, 3, length=300)
 y = erfcx.(-x)
 
 Nvals = collect(4:2:10)
@@ -16,19 +16,21 @@ function draw_errors(contour::Symbol)
         N = Nvals[row]
         E = MLQuad(α, β, N, contour)
         err = abs.( y - E.(x) )
-        semilogy(x[2:end], err[2:end], label="N = $N") # omit x=0
+        semilogy(x, err, label="N = $N") 
     end
-legend(loc="upper right")
-grid(true)
-xlabel(L"$x$")
+    legend(loc="upper right")
+    grid(true)
+    xlabel(L"$x$")
 end
+
+figure(1)
+draw_errors(:parabola)
+xylims = (-5.4, 3.4, 3e-12, 6e-4)
+axis(xylims)
+savefig("figure7L.pdf")
 
 figure(2)
 draw_errors(:hyperbola)
 savefig("figure7R.pdf")
-xylims = axis()
-
-figure(1)
-draw_errors(:parabola)
 axis(xylims)
-savefig("figure7L.pdf")
+
